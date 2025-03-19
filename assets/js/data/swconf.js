2e1dbaf9528638fe57743cd6ef933c5af7e2b1f0
dineshloganathan395 <dineshloganathan395@users.noreply.github.com>
2025-03-19 08:02:53 +0000

    deploy: 201bb3e4a27bcbe5522412bc32957ef098bde0c4

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742370975',resources: [
+    cacheName: 'chirpy-1742371372',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
